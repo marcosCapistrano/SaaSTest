ef7bc3377537cd7f0c47c73e44625c2d876e5c63
Marcos Capistrano <mcapistrano50@gmail.com>
2021-09-01 11:21:54 -0300

    feat(project): teste

--- api/server/server.ts
+++ api/server/server.ts
@@ -1,16 +1,11 @@
-import * as express from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
 
-// eslint-disable-next-line
-require('dotenv').config();
+dotenv.config();
 
 const server = express();
 
 server.use(express.json());
-
-server.get('/api/v1/public/get-user', (_, res) => {
-	console.log('API server got request from APP server or browser');
-	res.json({ user: { email: 'team@builderbook.org' } });
-});
 
 server.get('*', (_, res) => {
 	res.sendStatus(403);
